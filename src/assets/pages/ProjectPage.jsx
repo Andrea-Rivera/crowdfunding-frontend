2147d9a1838add50576a67f9ffc6a00a33ba74f3
Andrea <andita54@hotmail.com>
2023-10-07 20:36:48 +1000

    user permissions

--- src/assets/pages/ProjectPage.jsx
+++ src/assets/pages/ProjectPage.jsx
@@ -5,6 +5,7 @@ import deleteProject from "../../api/delete-project";
 import deletePledge from "../../api/delete-pledge";
 import { useNavigate } from 'react-router-dom'
 import Button from "../components/Buttton/Button";
+import { useAuth } from "../../hooks/use-auth";
 import "./ProjectPage.css"
 
 
@@ -13,6 +14,7 @@ function ProjectPage() {
     const { id } = useParams();
     const navigate = useNavigate()
     const { project, isLoading, error } = useProject(id);
+    const {auth, setAuth} = useAuth();
 
 
     console.log(isLoading)
@@ -55,6 +57,9 @@ function ProjectPage() {
 
     return (
         <div>
+          
+            {auth.token ? (
+                  <>
             <section className="projectList">
             <h2>{project.title}</h2>
             <Button text={"Edit Project"} btnClass = "btn-info "  onClick={handleEditSubmit}/>
@@ -80,9 +85,13 @@ function ProjectPage() {
             </section>
             <section className="createPledge">
 
-<CreatePledge projectId={id}/>
-</section>
-</div>
+            <CreatePledge projectId={id}/>
+            </section>
+            </>
+            ) : (
+            <p>Please Log In</p>
+            )} 
+    </div>
     );
 }
 
