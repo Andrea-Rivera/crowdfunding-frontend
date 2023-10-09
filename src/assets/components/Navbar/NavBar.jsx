0bb87d29e9d6120aecd055e6d178a8029c627fe0
Andrea <andita54@hotmail.com>
2023-10-09 15:42:45 +1000

    updating css for navbar and footer

--- src/assets/components/Navbar/NavBar.jsx
+++ src/assets/components/Navbar/NavBar.jsx
@@ -3,6 +3,7 @@ import{ Link, Outlet }from"react-router-dom";
 import { useAuth } from "../../../hooks/use-auth";
 import {GiHamburgerMenu} from "react-icons/gi";
 import {FaTimes} from "react-icons/fa";
+import Footer from "../Footer/Footer";
 import "./NavBar.css";
 
 
@@ -17,14 +18,14 @@ const NavBar = () =>
 
     const [isMobile, setIsMobile] = useState(false)
     return (
-    <div>
+    <>
         <nav className="navbar">
         <ul className={isMobile ? "nav-links-mobile":"nav-links"}
 onClick = {()=>  setIsMobile(false)}>
             <Link to="/" className="home"> <li>Home</li></Link>
             {auth.token ? (
                    <>
-                <Link to="/" className="login" onClick={handleLogout}>
+                <Link to="/" className="logout" onClick={handleLogout}>
                 <li>Log Out</li>
                 </Link>
                 <Link to="/projects" className="project"><li>Create Project</li></Link>
@@ -38,8 +39,9 @@ onClick = {()=>  setIsMobile(false)}>
       {isMobile?(<i><FaTimes/> </i>):(<i ><GiHamburgerMenu/></i>)}
     </button>
         </nav>
-        <Outlet />    
-    </div>  
+        <Outlet />  
+        <Footer/>  
+    </>  
     );
 }
 
